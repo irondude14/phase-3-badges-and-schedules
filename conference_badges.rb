101022b8d6f3d4035e9cd4035c62937d2a7c4ea6
Vladyslav <vkadyrov@protonmail.com>
2023-04-04 16:21:01 -0400

    commit tree 7c099743242929d7d111f19a3a894c112a2ea838

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map { |name| "Hello, my name is #{name}." }
+end
+
+def assign_rooms(array)
+  assignments = []
+  array.each_with_index do |name, index|
+    room_number = index + 1
+    message = "Hello, #{name}! You'll be assigned to room #{room_number}!"
+    assignments << message
+  end
+  assignments
+end
+
+def printer(array)
+  beadges = batch_badge_creator(array).each { |item| puts item }
+
+  room_assignments = assign_rooms(array).each { |item| puts item }
+
+  beadges
+  room_assignments
+end
